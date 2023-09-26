016bd960c36bee39de238d765a8e854a333d8fa7
Francisco Gabriel <franciscogabrielf11@gmail.com>
2023-09-26 16:08:06 -0300

    feat : second version of ui

--- lib/sign_in_page.dart
+++ lib/sign_in_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'custom_text_field.dart';
 
@@ -27,7 +28,7 @@ class SignInPage extends StatelessWidget {
                 const SizedBox(height: 20),
                 Text(
                   "Welcome back you've been missed!",
-                  style: TextStyle(
+                  style: GoogleFonts.rubik(
                     color: Colors.grey[600],
                     fontSize: 16,
                     fontWeight: FontWeight.bold,
@@ -37,6 +38,66 @@ class SignInPage extends StatelessWidget {
                 const CustomTextField(label: 'Username'),
                 const SizedBox(height: 16),
                 const CustomTextField(label: 'Password', isSecret: true),
+                const SizedBox(height: 10),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    "Forgot Password?",
+                    style: GoogleFonts.rubik(
+                      color: Colors.grey[700],
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 35),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(double.infinity, 80),
+                    backgroundColor: Colors.black,
+                    shape: ContinuousRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                  ),
+                  child: Text(
+                    'Sign In',
+                    style: GoogleFonts.rubik(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 50),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                      width: 110,
+                      child: Divider(
+                        color: Colors.grey[600],
+                        thickness: 0.5,
+                      ),
+                    ),
+                    Text(
+                      "Or continue with",
+                      style: GoogleFonts.rubik(
+                        color: Colors.grey[600],
+                        fontSize: 16,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 110,
+                      child: Divider(
+                        color: Colors.grey[600],
+                        thickness: 0.5,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 50),
+                Row()
               ],
             ),
           ),
