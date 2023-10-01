dfe9ef6a57411c63d4a98eaa0d69a4343b75a8a3
Francisco Gabriel <franciscogabrielf11@gmail.com>
2023-10-01 15:31:13 -0300

    feat : setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
+import 'package:auth_boilerplate/firebase_options.dart';
 import 'package:device_preview/device_preview.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'view/pages/sign_in_page.dart';
 
-void main() {
-  runApp(DevicePreview(
-    enabled: true,
-    builder: (context) => const MainApp(),
-  ));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) => const MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
