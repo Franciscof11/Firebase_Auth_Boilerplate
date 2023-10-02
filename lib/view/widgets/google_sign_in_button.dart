139c7f9b58abafa81e118022b0af406947441b25
Francisco Gabriel <franciscogabrielf11@gmail.com>
2023-10-02 16:33:53 -0300

    refac : google sign button

--- lib/view/widgets/google_sign_in_button.dart
+++ lib/view/widgets/google_sign_in_button.dart
@@ -1,7 +1,7 @@
 import 'package:auth_boilerplate/controller/google_sign_in.dart';
 import 'package:flutter/material.dart';
 
-class GoogleSignInButton extends StatelessWidget {
+class GoogleSignInButton extends StatefulWidget {
   final String urlImage;
 
   const GoogleSignInButton({
@@ -10,18 +10,29 @@ class GoogleSignInButton extends StatelessWidget {
   });
 
   @override
+  State<GoogleSignInButton> createState() => _GoogleSignInButtonState();
+}
+
+class _GoogleSignInButtonState extends State<GoogleSignInButton> {
+  @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => signInWithGoogle(),
+      onTap: () {
+        signInWithGoogle();
+        const CircularProgressIndicator();
+        setState(() {});
+      },
       child: Container(
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(16),
             color: Colors.grey[200],
-            border: Border.all(color: Colors.white)),
+            border: Border.all(
+              color: Colors.white,
+            )),
         child: Padding(
           padding: const EdgeInsets.all(10),
           child: Image.asset(
-            urlImage,
+            widget.urlImage,
             height: 55,
           ),
         ),
